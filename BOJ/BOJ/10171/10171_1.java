cfc22e944c5d4e25a18a5246794420c49fdac52b
도모 <dongho5088@naver.com>
2022-08-05 22:01:22 +0900

    BOJ-10171 '고양이' [ 22/08/05 22:01:22 ] by Alub

---
+++ BOJ/BOJ/10171/10171_1.java
@@ -0,0 +1,13 @@
+/**
+* 메모리: 14108 KB, 시간: 120 ms
+* 2022.08.05
+* by Alub
+*/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
