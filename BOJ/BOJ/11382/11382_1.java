36d42126aa17c2ea25a2dd6791cd37bb77e9254e
도모 <dongho5088@naver.com>
2022-08-05 22:07:19 +0900

    BOJ-11382 '꼬마 정민' [ 22/08/05 22:07:19 ] by Alub

---
+++ BOJ/BOJ/11382/11382_1.java
@@ -0,0 +1,14 @@
+/**
+* 메모리: 17704 KB, 시간: 212 ms
+* 2022.08.05
+* by Alub
+*/
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(sc.nextLong() + sc.nextLong() + sc.nextLong());
+    }
+}
