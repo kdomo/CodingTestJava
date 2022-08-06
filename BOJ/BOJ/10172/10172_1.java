7a8b7a80f9a12ca9b93bcda4d8e3d8dd8cfe3d89
도모 <dongho5088@naver.com>
2022-08-06 11:57:58 +0900

    BOJ-10172 '개' [ 22/08/06 11:57:58 ] by Alub

---
+++ BOJ/BOJ/10172/10172_1.java
@@ -0,0 +1,14 @@
+/**
+* 메모리: 14032 KB, 시간: 128 ms
+* 2022.08.06
+* by Alub
+*/
+public class Main{
+     public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
