929095297061fa9939fc76ccbf06363f73ce9854
도모 <dongho5088@naver.com>
2022-08-06 15:18:46 +0900

    BOJ-2741 'N 찍기' [ 22/08/06 15:18:46 ] by Alub

---
+++ BOJ/2741/2741_1.java
@@ -0,0 +1,16 @@
+/**
+* 메모리: 31244 KB, 시간: 1032 ms
+* 2022.08.06
+* by Alub
+*/
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for (int i = 1; i <= N; i++) {
+            System.out.println(i);
+        }
+    }
+}
