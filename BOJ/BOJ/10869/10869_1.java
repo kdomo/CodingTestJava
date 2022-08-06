75a0aa04815386687f85b7486b0f270b610fae65
도모 <dongho5088@naver.com>
2022-08-06 12:05:36 +0900

    BOJ-10869 '사칙연산' [ 22/08/06 12:05:36 ] by Alub

---
+++ BOJ/BOJ/10869/10869_1.java
@@ -0,0 +1,19 @@
+/**
+* 메모리: 17732 KB, 시간: 224 ms
+* 2022.08.06
+* by Alub
+*/
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
