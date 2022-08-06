6ff3f72aca58306fc065a136deaacd4e90a9dfe3
도모 <dongho5088@naver.com>
2022-08-06 12:02:40 +0900

    BOJ-1001 'A-B' [ 22/08/06 12:02:40 ] by Alub

---
+++ BOJ/BOJ/1001/1001_1.java
@@ -0,0 +1,12 @@
+/**
+* 메모리: 17720 KB, 시간: 220 ms
+* 2022.08.06
+* by Alub
+*/
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt() - sc.nextInt());
+    }
+}
