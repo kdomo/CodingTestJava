319373d20f95723ed8c12d8f0c03d7b2acd62ab0
도모 <dongho5088@naver.com>
2022-08-06 12:09:34 +0900

    BOJ-1008 'A/B' [ 22/08/06 12:09:34 ] by Alub

---
+++ BOJ/BOJ/1008/1008_1.java
@@ -0,0 +1,15 @@
+/**
+* 메모리: 17848 KB, 시간: 228 ms
+* 2022.08.06
+* by Alub
+*/
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.printf("%.9f",(double)a/b);
+    }
+}
