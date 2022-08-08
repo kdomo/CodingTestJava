eaeb2da26342bca99acce3f058406bb15074bc3e
도모 <dongho5088@naver.com>
2022-08-08 15:11:26 +0900

    BOJ-10951 'A+B - 4' [ 22/08/08 15:11:26 ] by Alub

---
+++ BOJ/10951/10951_1.java
@@ -0,0 +1,18 @@
+/**
+* 메모리: 17920 KB, 시간: 228 ms
+* 2022.08.08
+* by Alub
+*/
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+      
+        while(sc.hasNext()){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a+b);
+        }
+    }
+}
