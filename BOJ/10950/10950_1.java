8cae5c0b78578747bc0fe1b81e3b0bed79930329
도모 <dongho5088@naver.com>
2022-08-08 15:13:06 +0900

    BOJ-10950 'A+B - 3' [ 22/08/08 15:13:06 ] by Alub

---
+++ BOJ/10950/10950_1.java
@@ -0,0 +1,18 @@
+/**
+* 메모리: 17824 KB, 시간: 248 ms
+* 2022.08.08
+* by Alub
+*/
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0; i<n; i++){
+             int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a+b);
+        }
+    }
+}
