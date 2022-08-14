fa001f7a374bdbe6e2b9b26da1660cf2b06964bd
도모 <dongho5088@naver.com>
2022-08-14 13:55:23 +0900

    BOJ-11654 '아스키 코드' [ 22/08/14 13:55:22 ] by Alub

---
+++ BOJ/11654/11654_1.java
@@ -0,0 +1,14 @@
+/**
+* 메모리: 17652 KB, 시간: 204 ms
+* 2022.08.14
+* by Alub
+*/
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        System.out.println(a.charAt(0)+0);
+    }
+}
