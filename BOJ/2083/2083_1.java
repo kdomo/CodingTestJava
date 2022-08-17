70749a546b95a44aff3de537c25f6c63bbb60a9f
도모 <dongho5088@naver.com>
2022-08-17 16:29:37 +0900

    BOJ-2083 '럭비 클럽' [ 22/08/17 16:29:37 ] by Alub

---
+++ BOJ/2083/2083_1.java
@@ -0,0 +1,27 @@
+/**
+* 메모리: 17652 KB, 시간: 208 ms
+* 2022.08.17
+* by Alub
+*/
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String str = sc.next();
+            int age = sc.nextInt();
+            int weight = sc.nextInt();
+
+            if (str.equals("#") && age == 0 && weight ==0) {
+                break;
+            }
+
+            if (age > 17 || weight >= 80) {
+                System.out.println(str +" "+"Senior");
+            } else  {
+                System.out.println(str +" "+"Junior");
+            }
+        }
+    }
+}
