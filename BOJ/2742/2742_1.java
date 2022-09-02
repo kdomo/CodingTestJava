daf8053addb9855c71a2ca52cd3ba2a60d91d60c
도모 <dongho5088@naver.com>
2022-09-02 10:56:42 +0900

    BOJ-2742 '기찍 N' [ 22/09/02 10:56:42 ] by Alub

---
+++ BOJ/2742/2742_1.java
@@ -0,0 +1,16 @@
+/**
+* 메모리: 33624 KB, 시간: 1124 ms
+* 2022.09.02
+* by Alub
+*/
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for (int i = N; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+}
