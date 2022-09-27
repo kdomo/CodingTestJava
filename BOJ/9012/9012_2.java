3d7aa9f08343bbcc299d33f67dd70041feee6fea
도모 <dongho5088@naver.com>
2022-09-27 21:37:32 +0900

    BOJ-9012 '괄호' [ 22/09/27 21:37:32 ] by Alub

---
+++ BOJ/9012/9012_2.java
@@ -0,0 +1,41 @@
+/**
+* 메모리: 14304 KB, 시간: 136 ms
+* 2022.09.27
+* by Alub
+*/
+import java.io.*;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int k = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < k; i++) {
+            sb.append(stack(br.readLine())).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+    static String stack(String s){
+        Stack<Character> st = new Stack<>();
+
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+
+            if(c == '('){
+                st.push(c);
+            }else if(st.empty()){
+                return "NO";
+            }else{
+                st.pop();
+            }
+        }
+        if(st.empty()){
+            return "YES";
+        }else{
+            return "NO";
+        }
+    }
+}
