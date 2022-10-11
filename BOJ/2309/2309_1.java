387222a7245fd840b65b0a429203e3b8e9b30443
도모 <dongho5088@naver.com>
2022-10-11 18:18:38 +0900

    BOJ-2309 '일곱 난쟁이' [ 22/10/11 18:18:37 ] by Alub

---
+++ BOJ/2309/2309_1.java
@@ -0,0 +1,41 @@
+/**
+* 메모리: 17672 KB, 시간: 208 ms
+* 2022.10.11
+* by Alub
+*/
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[9];
+        int sum = 0;
+        int a = 0;
+        int b = 0;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+            sum += arr[i];
+        }
+        Arrays.sort(arr);
+
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (sum - arr[i] - arr[j] == 100) {
+                    a = i;
+                    b = j;
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (i != a && i != b) {
+                System.out.println(arr[i]);
+            }
+        }
+    }
+}
