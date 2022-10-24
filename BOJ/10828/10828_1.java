5112ebb6dd15a7f707431885e7da17811bb7d37e
도모 <dongho5088@naver.com>
2022-10-24 19:55:23 +0900

    BOJ-10828 '스택' [ 22/10/24 19:55:23 ] by Alub

---
+++ BOJ/10828/10828_1.java
@@ -0,0 +1,53 @@
+/**
+* 메모리: 22596 KB, 시간: 228 ms
+* 2022.10.24
+* by Alub
+*/
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<String> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            String cmd = str.split(" ")[0];
+            switch (cmd) {
+                case "push":
+                    String value = str.split(" ")[1];
+                    stack.push(value);
+                    break;
+                case "pop":
+                    if (stack.isEmpty()) {
+                        sb.append("-1\n");
+                    } else {
+                        sb.append(stack.pop() + "\n");
+                    }
+                    break;
+                case "size":
+                    sb.append(stack.size() + "\n");
+                    break;
+                case "empty" :
+                    if(stack.isEmpty()){
+                        sb.append("1\n");
+                    }else{
+                        sb.append("0\n");
+                    }
+                    break;
+                case "top":
+                    if(stack.isEmpty()){
+                        sb.append("-1\n");
+                    }else{
+                        sb.append(stack.peek()+"\n");
+                    }
+                    break;
+            }
+        }
+        System.out.println(sb);
+    }
+}
