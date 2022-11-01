4cc68636a23e337de103d20601b1a114cbd22488
도모 <dongho5088@naver.com>
2022-11-01 21:17:49 +0900

    [Bronze I] Title: 이항 계수 1, Time: 124 ms, Memory: 14316 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int result = 1;
+        for (int i = 0; i < K; i++) {
+            result *= N--;
+        }
+        for (int i = K; i > 0; i--) {
+            result /= i;
+        }
+        System.out.println(result);
+    }
+}
