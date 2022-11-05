ae3c5d75c6c5fdb84001c70f655411911f4d7266
도모 <dongho5088@naver.com>
2022-11-05 23:52:32 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 128 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main{
+    public static void main(String[] agrs) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int y = Integer.parseInt(br.readLine());
+        System.out.println(y-543);
+    }
+}
