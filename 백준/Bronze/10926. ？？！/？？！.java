ac4cc7b591fb7438809310c3ea0584d15a7334bd
도모 <dongho5088@naver.com>
2022-11-05 23:49:07 +0900

    [Bronze V] Title: ??!, Time: 132 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,9 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main{
+    public static void main(String[] agrs) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine() + "??!");
+    }
+}
